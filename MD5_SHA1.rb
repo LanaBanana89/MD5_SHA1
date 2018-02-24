9a21a7ad75cbdf9b4be22dee3ea81fa559cda6e7
LanaBanana89 <32016884+LanaBanana89@users.noreply.github.com>
2018-02-24 19:36:58 +0300

    add comments

--- MD5_SHA1.rb
+++ MD5_SHA1.rb
@@ -1,5 +1,5 @@
 #программа, шифрующая слова и фразы с помощью MD5 и SHA-1
-
+#код для распознавания русских букв на windows
 if (Gem.win_platform?)
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -9,30 +9,27 @@ if (Gem.win_platform?)
   end
 end
 
+#подключаем гем digest
 require 'digest'
 
 puts "Введите слово или фразу для шифрования"
-
-  word = STDIN.gets.encode("UTF-8").chomp
+word = STDIN.gets.encode("UTF-8").chomp
 
 puts "Каким способом зашифровать: \n1.MD5\n2.SHA1\n3.SHA2"
+num = STDIN.gets.to_i
 
-  num = STDIN.gets.to_i
+until([1,2,3].include?(num)) do
+  puts "\nКаким способом зашифровать:\n1. MD5\n2. SHA1\n3. SHA2"
+end
 
-
-  until([1,2,3].include?(num)) do
-    puts "\nКаким способом зашифровать:\n1. MD5\n2. SHA1\n3. SHA2"
-
-  case num
-    when 1
-    cipher = Digest::MD5.hexdigest(word)
-
-    when 2
-    cipher = Digest::SHA1.hexdigest(word)
-
-    when 3
-    cipher = Digest::SHA2.hexdigest(word)
-
-  end
-    puts "Вот что получилось: " + cipher
+case num
+when 1
+  cipher = Digest::MD5.hexdigest(word)
+when 2
+  cipher = Digest::SHA1.hexdigest(word)
+when 3
+  cipher = Digest::SHA2.hexdigest(word)
+end
+  
+puts "Вот что получилось: " + cipher
 
